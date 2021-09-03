7e4afdd2b4e0706c6c1f9088dff629b8deb49a0b
Libor Gabaj <libor.gabaj@gmail.com>
2021-09-03 18:40:46 +0200

    Added prefixing.

--- README.md
+++ README.md
@@ -1,29 +1,38 @@
 <a id="library"></a>
-# gbjSerialDebug
+# gbj\_serial\_debug
 Library defines preprocessor macro functions for serial debugging and writing to the serial port.
 
 
 <a id="constants"></a>
 ## Constants
+
 - **GBJ\_SERIAL\_DEBUG\_VERSION**: Name and semantic version of the library.
-- **SERIAL\_NODEBUG**: Defined this debugging macro determines, that serial debugging should not be used.
+- **SERIAL\_NODEBUG**: Defining this macro determines, that serial debugging should not be used.
+- **SERIAL\_PREFIX**: Macro defines a string, that is prefixed to logging outputs with internally define separator, which is `::`. The macro is useful for debugging output in application libraries. In order to avoid compilation warnings about redefining this macro, it is usefull to undefine it right before defining it again for the desired prefix.
 
-*The debugging macros should be defined before inclusion of the library header file in a sketch in order to take them into effect.*
+> The debugging macros should be defined before inclusion of the library header file in a sketch in order to take them into effect.
 
-Those concept of debugging constants ensures that by default the debugging is active if none of those constants is defined. Exact debugging mode can be defined by defining respective macro.
-
-Remaining constants are listed in the library include file. They are used mostly internally.
+```cpp
+//#define SERIAL_NODEBUG
+#undef SERIAL_PREFIX
+#define SERIAL_PREFIX "mylib"
+...
+#include "gbj_serial_debug.h"
+```
 
 
 <a id="interface"></a>
 ## Interface
-- All macro functions are called without trailing semicolon.
+
+- All macro functions can be called without trailing semicolon. However, to omit problems with formatters, it is recommended to use them.
 - Macro functions with parameters are called as ordinary C functions with braces.
 - Macro functions without parameters are called without braces like macro constants.
+
 
 #### System macros
 - **SERIAL\_BEGIN(b)**: Starts serial port with baud speed as a parameter, e.g., `SERIAL_BEGIN(9600)`.
 - **SERIAL\_F(s)**: Places string to the flash memory if the platform allows it, e.g., `send(SERIAL_F("Text"));`.
+
 
 #### Login macros
 - **SERIAL\_LOG1(p1)**: Prints one parameter on the separate line with EOL (end of line), e.g., `SEND_LOG1(123)`.
@@ -33,6 +42,7 @@ Remaining constants are listed in the library include file. They are used mostly
 - **SERIAL\_LOG4(p1, p2, p3, p4, p5)**: ditto
 - **SERIAL\_LOG6(p1, p2, p3, p4, p5, p6)**: ditto
 
+
 #### Chain macros
 - **SERIAL\_CHAIN1(p1)**: Prints one parameter on current line without EOL, e.g., `SEND_CHAIN1(123)`, which enables chaining multiple strings with macro. After chaining all strings it is useful to use some of login macros.
 - **SERIAL\_CHAIN2(p1, p2)**: Prints both parameters on current line without EOL, e.g., `SEND_CHAIN2("T=", 123)`.
@@ -41,13 +51,16 @@ Remaining constants are listed in the library include file. They are used mostly
 - **SERIAL\_CHAIN4(p1, p2, p3, p4, p5)**: ditto
 - **SERIAL\_CHAIN6(p1, p2, p3, p4, p5, p6)**: ditto
 
+
 #### Debug macros
 - **SERIAL\_TITLE(s)**: Flashes input string and prints it on the separate line with EOL, e.g., `SEND_TITLE("Start")`.
 - **SERIAL\_ACTION(s)**: Flashes input string and prints it on the separate line without EOL. It is useful when another printing on the same line is expected, e.g., by waiting on connection and signaling it by dots.
+- **SERIAL\_ACTION\_END(s)**: Continues by flashing input string to the recent line without prefixing the string. It finishes an action.
 - **SERIAL\_VALUE(k, v)**: Flashes and prints the first string parameter (key) and follows it with second parameter (value) with EOL, e.g., `SEND_VALUE("Value", 85)`. The macro adds colon and space after the first parameter and forms key-value pair output.
 - **SERIAL\_VALUE\_UNIT(k, v, u)**: Flashes and prints the first string parameter (key), follows it with second parameter (value), and appends the third string parameter (unit) with EOL, e.g., `SEND_VALUE_UNIT("Value", 85, "degC")`. The macro adds colon and space after the first parameter, space before the last parameter, and forms key-value-unit output.
 
+
 #### Decoration macros
-- **SERIAL\_LINE**: Prints empty line with EOL separating printing sections, e.g., `SERIAL_LINE`.
+- **SERIAL\_LINE**: Prints empty line with EOL separating printing sections.
 - **SERIAL\_DELIM**: Flashes and prints three dashes as a delimiter line and EOL for visual separating printing sections.
 - **SERIAL\_DOT**: Flashes and prints dot without EOL for mimicking waiting for long lasting actions.
--- src/gbj_serial_debug.h
+++ src/gbj_serial_debug.h
@@ -16,72 +16,189 @@
  */
 #ifndef GBJ_SERIAL_DEBUG_H
 #define GBJ_SERIAL_DEBUG_H
-#define GBJ_SERIAL_DEBUG_VERSION "GBJ_SERIAL_DEBUG 1.0.0"
+#define GBJ_SERIAL_DEBUG_VERSION "GBJ_SERIAL_DEBUG 1.1.0"
 
 #if defined(__AVR__)
-  #if ARDUINO >= 100
-    #include <Arduino.h>
-  #else
-    #include <WProgram.h>
-  #endif
-  #include <inttypes.h>
-  #define SERIAL_F(s) F(s)
+#include <Arduino.h>
+#include <inttypes.h>
+#define SERIAL_F(s) F(s)
 #elif defined(ESP8266) || defined(ESP32)
-  #include <Arduino.h>
-  #define SERIAL_F(s) F(s)
+#include <Arduino.h>
+#define SERIAL_F(s) F(s)
 #elif defined(PARTICLE)
-  #include <Particle.h>
-  #define SERIAL_F(s) s
+#include <Particle.h>
+#define SERIAL_F(s) s
 #else
-  #define SERIAL_F(s) s
+#define SERIAL_F(s) s
 #endif
 
 #if !defined(SERIAL_NODEBUG)
-  #define SERIAL_LOG1(p1)                     { Serial.println(p1); }
-  #define SERIAL_LOG2(p1, p2)                 { Serial.print(p1); Serial.println(p2); }
-  #define SERIAL_LOG3(p1, p2, p3)             { Serial.print(p1); Serial.print(p2); Serial.println(p3); }
-  #define SERIAL_LOG4(p1, p2, p3, p4)         { Serial.print(p1); Serial.print(p2); Serial.print(p3); Serial.println(p4); }
-  #define SERIAL_LOG5(p1, p2, p3, p4, p5)     { Serial.print(p1); Serial.print(p2); Serial.print(p3); Serial.print(p4); Serial.println(p5); }
-  #define SERIAL_LOG6(p1, p2, p3, p4, p5, p6) { Serial.print(p1); Serial.print(p2); Serial.print(p3); Serial.print(p4); Serial.print(p5); Serial.println(p6); }
-  //
-  #define SERIAL_CHAIN1(p1)                     { Serial.print(p1); }
-  #define SERIAL_CHAIN2(p1, p2)                 { Serial.print(p1); Serial.print(p2); }
-  #define SERIAL_CHAIN3(p1, p2, p3)             { Serial.print(p1); Serial.print(p2); Serial.print(p3); }
-  #define SERIAL_CHAIN4(p1, p2, p3, p4)         { Serial.print(p1); Serial.print(p2); Serial.print(p3); Serial.print(p4); }
-  #define SERIAL_CHAIN5(p1, p2, p3, p4, p5)     { Serial.print(p1); Serial.print(p2); Serial.print(p3); Serial.print(p4); Serial.print(p5); }
-  #define SERIAL_CHAIN6(p1, p2, p3, p4, p5, p6) { Serial.print(p1); Serial.print(p2); Serial.print(p3); Serial.print(p4); Serial.print(p5); Serial.print(p6); }
-  //
-  #define SERIAL_LINE                         { Serial.println(); }
-  #define SERIAL_BEGIN(b)                     { Serial.begin(b); while(!Serial) { }; SERIAL_LINE }
-  #define SERIAL_TITLE(s)                     { SERIAL_LOG1(SERIAL_F(s)) }
-  #define SERIAL_VALUE(k, v)                  { SERIAL_LOG3(SERIAL_F(k), SERIAL_F(": "), v) }
-  #define SERIAL_VALUE_UNIT(k, v, u)          { SERIAL_LOG5(SERIAL_F(k), SERIAL_F(": "), v, SERIAL_F(" "), SERIAL_F(u)) }
-  #define SERIAL_DELIM                        { SERIAL_TITLE("---") }
-  #define SERIAL_ACTION(s)                    { Serial.print(SERIAL_F(s)); }
-  #define SERIAL_DOT                          { Serial.print(SERIAL_F(".")); }
+
+#ifdef SERIAL_PREFIX
+#define SERIAL_SEPAR_H SERIAL_F("::")
 #else
-  #define SERIAL_LOG1(p1)
-  #define SERIAL_LOG2(p1, p2)
-  #define SERIAL_LOG3(p1, p2, p3)
-  #define SERIAL_LOG4(p1, p2, p3, p4)
-  #define SERIAL_LOG5(p1, p2, p3, p4, p5)
-  #define SERIAL_LOG6(p1, p2, p3, p4, p5, p6)
-  //
-  #define SERIAL_CHAIN1(p1)
-  #define SERIAL_CHAIN2(p1, p2)
-  #define SERIAL_CHAIN3(p1, p2, p3)
-  #define SERIAL_CHAIN4(p1, p2, p3, p4)
-  #define SERIAL_CHAIN5(p1, p2, p3, p4, p5)
-  #define SERIAL_CHAIN6(p1, p2, p3, p4, p5, p6)
-  //
-  #define SERIAL_LINE
-  #define SERIAL_BEGIN(b)
-  #define SERIAL_TITLE(s)
-  #define SERIAL_VALUE(k, v)
-  #define SERIAL_VALUE_UNIT(k, v, u)
-  #define SERIAL_DELIM
-  #define SERIAL_ACTION(s)
-  #define SERIAL_DOT
+#define SERIAL_PREFIX ""
+#define SERIAL_SEPAR_H ""
+#endif
+
+#define SERIAL_LOG1(p1)                    \
+  {                                        \
+    Serial.print(SERIAL_F(SERIAL_PREFIX)); \
+    Serial.print(SERIAL_SEPAR_H);          \
+    Serial.println(p1);                    \
+  }
+#define SERIAL_LOG2(p1, p2)                \
+  {                                        \
+    Serial.print(SERIAL_F(SERIAL_PREFIX)); \
+    Serial.print(SERIAL_SEPAR_H);          \
+    Serial.print(p1);                      \
+    Serial.println(p2);                    \
+  }
+#define SERIAL_LOG3(p1, p2, p3)            \
+  {                                        \
+    Serial.print(SERIAL_F(SERIAL_PREFIX)); \
+    Serial.print(SERIAL_SEPAR_H);          \
+    Serial.print(p1);                      \
+    Serial.print(p2);                      \
+    Serial.println(p3);                    \
+  }
+#define SERIAL_LOG4(p1, p2, p3, p4)        \
+  {                                        \
+    Serial.print(SERIAL_F(SERIAL_PREFIX)); \
+    Serial.print(SERIAL_SEPAR_H);          \
+    Serial.print(p1);                      \
+    Serial.print(p2);                      \
+    Serial.print(p3);                      \
+    Serial.println(p4);                    \
+  }
+#define SERIAL_LOG5(p1, p2, p3, p4, p5)    \
+  {                                        \
+    Serial.print(SERIAL_F(SERIAL_PREFIX)); \
+    Serial.print(SERIAL_SEPAR_H);          \
+    Serial.print(p1);                      \
+    Serial.print(p2);                      \
+    Serial.print(p3);                      \
+    Serial.print(p4);                      \
+    Serial.println(p5);                    \
+  }
+#define SERIAL_LOG6(p1, p2, p3, p4, p5, p6) \
+  {                                         \
+    Serial.print(SERIAL_F(SERIAL_PREFIX));  \
+    Serial.print(SERIAL_SEPAR_H);           \
+    Serial.print(p1);                       \
+    Serial.print(p2);                       \
+    Serial.print(p3);                       \
+    Serial.print(p4);                       \
+    Serial.print(p5);                       \
+    Serial.println(p6);                     \
+  }
+//
+#define SERIAL_CHAIN1(p1) \
+  {                       \
+    Serial.print(p1);     \
+  }
+#define SERIAL_CHAIN2(p1, p2) \
+  {                           \
+    Serial.print(p1);         \
+    Serial.print(p2);         \
+  }
+#define SERIAL_CHAIN3(p1, p2, p3) \
+  {                               \
+    Serial.print(p1);             \
+    Serial.print(p2);             \
+    Serial.print(p3);             \
+  }
+#define SERIAL_CHAIN4(p1, p2, p3, p4) \
+  {                                   \
+    Serial.print(p1);                 \
+    Serial.print(p2);                 \
+    Serial.print(p3);                 \
+    Serial.print(p4);                 \
+  }
+#define SERIAL_CHAIN5(p1, p2, p3, p4, p5) \
+  {                                       \
+    Serial.print(p1);                     \
+    Serial.print(p2);                     \
+    Serial.print(p3);                     \
+    Serial.print(p4);                     \
+    Serial.print(p5);                     \
+  }
+#define SERIAL_CHAIN6(p1, p2, p3, p4, p5, p6) \
+  {                                           \
+    Serial.print(p1);                         \
+    Serial.print(p2);                         \
+    Serial.print(p3);                         \
+    Serial.print(p4);                         \
+    Serial.print(p5);                         \
+    Serial.print(p6);                         \
+  }
+//
+#define SERIAL_LINE   \
+  {                   \
+    Serial.println(); \
+  }
+#define SERIAL_BEGIN(b) \
+  {                     \
+    Serial.begin(b);    \
+    while (!Serial)     \
+    {                   \
+    };                  \
+    SERIAL_LINE         \
+  }
+#define SERIAL_TITLE(s)      \
+  {                          \
+    SERIAL_LOG1(SERIAL_F(s)) \
+  }
+#define SERIAL_VALUE(k, v)                      \
+  {                                             \
+    SERIAL_LOG3(SERIAL_F(k), SERIAL_F(": "), v) \
+  }
+#define SERIAL_VALUE_UNIT(k, v, u)                                          \
+  {                                                                         \
+    SERIAL_LOG5(SERIAL_F(k), SERIAL_F(": "), v, SERIAL_F(" "), SERIAL_F(u)) \
+  }
+#define SERIAL_DELIM                 \
+  {                                  \
+    Serial.println(SERIAL_F("---")); \
+  }
+#define SERIAL_ACTION(s)                   \
+  {                                        \
+    Serial.print(SERIAL_F(SERIAL_PREFIX)); \
+    Serial.print(SERIAL_SEPAR_H);          \
+    Serial.print(SERIAL_F(s));             \
+  }
+#define SERIAL_ACTION_END(s)     \
+  {                              \
+    Serial.println(SERIAL_F(s)); \
+  }
+#define SERIAL_DOT               \
+  {                              \
+    Serial.print(SERIAL_F(".")); \
+  }
+#else
+#define SERIAL_LOG1(p1)
+#define SERIAL_LOG2(p1, p2)
+#define SERIAL_LOG3(p1, p2, p3)
+#define SERIAL_LOG4(p1, p2, p3, p4)
+#define SERIAL_LOG5(p1, p2, p3, p4, p5)
+#define SERIAL_LOG6(p1, p2, p3, p4, p5, p6)
+//
+#define SERIAL_CHAIN1(p1)
+#define SERIAL_CHAIN2(p1, p2)
+#define SERIAL_CHAIN3(p1, p2, p3)
+#define SERIAL_CHAIN4(p1, p2, p3, p4)
+#define SERIAL_CHAIN5(p1, p2, p3, p4, p5)
+#define SERIAL_CHAIN6(p1, p2, p3, p4, p5, p6)
+//
+#define SERIAL_LINE
+#define SERIAL_BEGIN(b)
+#define SERIAL_TITLE(s)
+#define SERIAL_VALUE(k, v)
+#define SERIAL_VALUE_UNIT(k, v, u)
+#define SERIAL_DELIM
+#define SERIAL_ACTION(s)
+#define SERIAL_ACTION_END(s)
+#define SERIAL_DOT
 #endif
 
 #endif
